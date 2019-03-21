2f805b73078851315f4c8fc7ea8ee2e7f27d1ae9
Pavly Remon <pavlyremon@pavlys-mac.local>
2019-03-21 11:32:48 +0200

    Initial Commit

---
+++ scrollerr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  scrollerr
+//
+//  Created by Pavly Remon on 3/21/19.
+//  Copyright © 2019 Pavly Remon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
